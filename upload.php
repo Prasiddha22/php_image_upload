3294cefc3652e9eb7c8b5974f21c6a5810d9b9ca
prasiddha22 <prasiddhaneupane22@gmail.com>
2022-08-23 12:40:04 +0545

    Comment Added

--- upload.php
+++ upload.php
@@ -10,7 +10,7 @@ if (isset($_FILES['image'])) {
     // Store image temp name
     $image_tmp_name = $image['tmp_name'];
 
-
+    // Store the image extension
     $image_ext = explode('.', $image_name);
     $image_first_name = $image_ext[0];
     $image_ext = strtolower(end($image_ext));
