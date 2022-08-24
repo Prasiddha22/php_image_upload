772e473b6856ca95bf2bef59002fbabe1cbdcfc7
prasiddha22 <prasiddhaneupane22@gmail.com>
2022-08-24 07:54:15 +0545

    Comment Added

--- compress_upload.php
+++ compress_upload.php
@@ -2,8 +2,9 @@
 
 // Check if the image is received or not
 if (isset($_FILES['image'])) {
-
+    // Store the received image details in variable
     $image = $_FILES['image'];
+
     $image_name = $image['name'];
     $image_tmp_name = $image['tmp_name'];
 
